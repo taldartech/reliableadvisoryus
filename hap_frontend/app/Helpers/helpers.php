df872f72acfe0bfae052d6ed9659d0e9e32de1b2
Shalin Devpura <shalindevpura19@gmail.com>
2026-04-03 12:02:08 +0530

    Removed HAP project

--- hap_frontend/app/Helpers/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (! function_exists('money_inr')) {
-    function money_inr(?float $amount): string
-    {
-        return '₹' . number_format((float) $amount, 2);
-    }
-}
--- hap_frontend/config/hap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'api_url' => env('HAP_API_URL', 'http://localhost:8000/api'),
-    'api_timeout' => (int) env('HAP_API_TIMEOUT', 15),
-];
